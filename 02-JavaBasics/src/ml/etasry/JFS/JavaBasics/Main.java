4ae5e166267528327ce395f92aad946a7b463c21
LER0ever <l.e.r@riseup.net>
2016-03-06 11:43:30 +0800

    02-JavaBasics

---
+++ 02-JavaBasics/src/ml/etasry/JFS/JavaBasics/Main.java
@@ -0,0 +1,14 @@
+package ml.etasry.JFS.JavaBasics;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Java Basics Example\n I am Israfil\n");
+
+        double pi = 3.1415926535897932384626;
+        int NumOfEggs = 2;
+        boolean InLove = false;
+
+	// write your code here
+    }
+}
