1da0964a2d124dd30a3235c411b558ac71831bb9
LER0ever <l.e.r@riseup.net>
2016-03-10 20:57:01 +0800

    Finished Lesson 02

--- 02-JavaBasics/src/ml/etasry/JFS/JavaBasics/Main.java
+++ 02-JavaBasics/src/ml/etasry/JFS/JavaBasics/Main.java
@@ -5,10 +5,27 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Java Basics Example\n I am Israfil\n");
 
+        //Java variables.
         double pi = 3.1415926535897932384626;
+        float height = (float)183.8; //Error for some reason
         int NumOfEggs = 2;
         boolean InLove = false;
 
-	// write your code here
+        //Final defines a constant: like const in C++
+        final double e = 2.7;
+        final int testfinal;
+        testfinal = 1; //can be assigned only once.
+
+        //transition between int and double
+        System.out.printf("3.0 / 4 = %f\n", 3.0/4);
+        System.out.printf("3 / 4.0 = %f\n", 3/4.0);
+        System.out.printf("(int)3.0 / 4 = %d\n", (int)3.0/4);
+        System.out.printf("(double)3 / 4 = %f\n", (double)3/4);
+        System.out.printf("(double)(3 / 4) = %f\n", (double)(3/4));
+
+	    // write your code here
     }
+
+
+
 }
